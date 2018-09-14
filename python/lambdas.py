7024d9034ce1489b431a094d6a33729f6d56486c
Ignacio Silva-Lepe <isilvalepe@gmail.com>
2018-09-14 16:51:57 -0500

    Complete the derivation of Y, issue #1

--- python/lambdas.py
+++ python/lambdas.py
@@ -37,6 +37,10 @@ f(3)(5) # => 15
 
 (lambda x,y : x*y)(3,5) # => 15, truly anonymous, no function names
 
+(lambda x: x+1)(3) # => 4
+# Is the same as:
+(lambda y: (lambda x: x+1)(y))(3) # => 4
+
 ##############
 
 f = lambda g,x : g(x)
@@ -72,9 +76,8 @@ fact = lambda x : 1 if x == 0 else fact(x-1) * x
 # Either way:
 fact(5) # => 120
 
-# Let's use an extra argument, when applied to anything it returns a function like fact
-# But the recursion keeps applying the dummy r
-fact_maker = lambda r : lambda x : 1 if x == 0 else fact_maker(r)(x-1) * x 
+# Let's use an extra argument that, when applied to anything, returns a function like fact
+fact_maker = lambda f : lambda x : 1 if x == 0 else fact_maker(f)(x-1) * x # But the recursion keeps applying the dummy f
 fact = fact_maker('foo')
 fact(5) # => 120
 
@@ -83,22 +86,74 @@ fact = fact_maker(fact_maker)
 fact(5) # => 120
 
 # This way, the recursion above also looks like fact_maker(fact_maker)
-# And we can just use r(r) as the recursion, as long as we call it with fact_maker(fact_maker) originally:
-fact_maker_maker = lambda r : lambda x : 1 if x == 0 else r(r)(x-1) * x
+fact_maker_maker = lambda f : lambda x : 1 if x == 0 else fact_maker_maker(fact_maker_maker)(x-1) * x
+# And we can just use f(f) as the recursion, as long as we call it with fact_maker(fact_maker) originally:
+fact_maker_maker = lambda f : lambda x : 1 if x == 0 else f(f)(x-1) * x
 fact = fact_maker_maker(fact_maker_maker)
 fact(5) # => 120
 
 # So, now we have lost the bound name in the recursion, and we only use args
+# Now, since:
+(lambda x: x+1)
+# is the same as:
+(lambda y: (lambda x: x+1)(y))
+# then:
+fact_maker_maker = lambda f : lambda x : 1 if x == 0 else (lambda a: f(f)(a))(x-1) * x
+fact = fact_maker_maker(fact_maker_maker)
+fact(5) # => 120
+
+# Also, since:
+(lambda y: (lambda x: x+1)(y))
+# is the same as:
+(lambda y: (lambda f: f(y))(lambda x: x+1))
+# then we can always abstract out (lambda a: f(f)(a)):
+fact_maker_maker = lambda f : (lambda r: lambda x : 1 if x == 0 else r(x-1) * x)(lambda a: f(f)(a))
+fact = fact_maker_maker(fact_maker_maker)
+fact(5) # => 120
+
+# Now, since (lambda r: lambda x : 1 if x == 0 else r(x-1) * x) is self contained
+# we can pull it out and assign to a variable:
+M = lambda r: lambda x : 1 if x == 0 else r(x-1) * x
+function_maker = lambda f : M(lambda a: f(f)(a)) # we can also rename fact_maker_maker
+fact = function_maker(function_maker) # as long as we keep using it consistently
+fact(5) # => 120
+
+# And since the value of function_maker does not depend on the name, then we can replace it in the definition of fact:
+M = lambda r: lambda x : 1 if x == 0 else r(x-1) * x
+fact = (lambda f : M(lambda a: f(f)(a)))(lambda f : M(lambda a: f(f)(a)))
+fact(5) # => 120
+
+# Finally, M can be generalized by abstracting it away inside a lambda:
+Y = lambda M: (lambda f : M(lambda a: f(f)(a)))(lambda f : M(lambda a: f(f)(a))) # This is (a variation of) the so-called Y combinator
+F = lambda r: lambda x : 1 if x == 0 else r(x-1) * x
+fact = Y(F)
+fact(5) # => 120
+
+# Or just:
+Y = lambda M: (lambda f : M(lambda a: f(f)(a)))(lambda f : M(lambda a: f(f)(a)))
+fact = Y(lambda r: lambda x : 1 if x == 0 else r(x-1) * x)
+fact(5) # => 120
+
+# Or even:
+Y = lambda M: (lambda f : M(lambda a: f(f)(a)))(lambda f : M(lambda a: f(f)(a)))
+Y(lambda r : lambda x : 1 if x == 0 else r(x-1) * x)(5) # => 120
+
+# Or even yet:
+(lambda M: (lambda f : M(lambda a: f(f)(a)))(lambda f : M(lambda a: f(f)(a))))(lambda r : lambda x : 1 if x == 0 else r(x-1) * x)(5) # => 120
 
 ##############
 
-Y = lambda M : (lambda f : M (lambda a : (f(f))(a)))(lambda f : M (lambda a : (f(f))(a)))
+# Exercise, count is defined as follows:
+def count(x):
+	return  0 if x == 0 else count(x-1)+1
 
+# Redefine count with calling itself
+
+# First, count can also be defined as:
 count = lambda x : 0 if x == 0 else count(x-1)+1
-lcount = Y(lambda rf : (lambda x : 0 if x == 0 else rf(x-1)+1))
-Y(lambda rf : (lambda x : 0 if x == 0 else rf(x-1)+1))(10) # => 10
 
-F = lambda r : lambda x : 1 if x == 0 else r(x-1) * x
-fact = Y(F)
-fact(4) # => 24
-Y(lambda r : lambda x : 1 if x == 0 else r(x-1) * x)(4) # => 24
+# Then:
+Y = lambda M: (lambda f : M(lambda a: f(f)(a)))(lambda f : M(lambda a: f(f)(a)))
+lcount = Y(lambda rf : (lambda x : 0 if x == 0 else rf(x-1)+1))
+lcount(10) # => 10
+Y(lambda rf : (lambda x : 0 if x == 0 else rf(x-1)+1))(10) # => 10
